54504b5ac96c555d35c2ac33cba7c2cab98b4801
yashpal.verma <yashpalverma@rocketmail.com>
2021-07-18 20:32:21 +0530

    'deployment'

---
+++ main.py
@@ -0,0 +1,45 @@
+from logging import debug
+
+from flask import Flask , render_template , request
+import joblib
+app=Flask(__name__)
+
+#load model
+model=joblib.load("hiring_model.pkl")
+
+@app.route('/')
+def welcome():
+    return render_template('base.html')
+
+@app.route('/predict' , methods=["POST"])
+def predict():
+    exp=request.form.get("experience")
+    score=request.form.get("test_score")
+    interview_score=request.form.get("Interview_score")
+
+    prediction=model.predict([[int(exp),int(score),int(interview_score)]])
+
+    output = round(prediction[0],2)
+
+    return render_template("base.html",prediction_text= f"Employee salary will be $ {output}")
+
+
+    return "YOUR FORM IS SUBMITTED"
+
+# @app.route('/contact')
+# def cont():
+#     return "welcome to contact page"
+
+# @app.route('/feedback')
+# def feed():
+#     return "welcome to feedback page"
+
+# @app.route('/help')
+# def help():
+#     return "welcome to help page"
+
+# @app.route('/data science')
+# def ds():
+#     return "welcome to data science page"
+
+app.run(debug=True)
---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import joblib
+
+from sklearn.linear_model import LinearRegression
+
+dataset=pd.read_csv("hiring.csv")
+print(dataset)
+dataset.experience.fillna(0,inplace=True)
+dataset.test_score.fillna(dataset.test_score.mean(),inplace=True)
+X=dataset.iloc[:,:3]
+def convert_to_int(word):
+    word_dic={"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eiht":8,"nine":9,"ten":10,"eleven":11,0:0}
+    return word_dic[word]
+
+X["experience"]=X.experience.apply(lambda x : convert_to_int(x))
+
+y=dataset.iloc[:,-1]
+
+regressor=LinearRegression()
+regressor.fit(X,y)
+print("Model training is done")
+
+joblib.dump(regressor,"hiring_model.pkl")
+print(regressor.predict([[1,8,9]]))
