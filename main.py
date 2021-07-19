faf1ad40aa3db1e172675bab615d2aec3581ffd6
yashpal.verma <yashpalverma@rocketmail.com>
2021-07-19 09:03:45 +0530

    07/19/2021

--- main.py
+++ main.py
@@ -1,45 +1,30 @@
 from logging import debug
-
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request
+import numpy as np
 import joblib
-app=Flask(__name__)
 
-#load model
-model=joblib.load("hiring_model.pkl")
+app = Flask(__name__)
+
+# load a model
+model = joblib.load('hiring_model.pkl')
 
 @app.route('/')
 def welcome():
     return render_template('base.html')
 
-@app.route('/predict' , methods=["POST"])
+@app.route('/predict', methods = ['POST'])
 def predict():
-    exp=request.form.get("experience")
-    score=request.form.get("test_score")
-    interview_score=request.form.get("Interview_score")
 
-    prediction=model.predict([[int(exp),int(score),int(interview_score)]])
+    exp = request.form.get('experience')
+    score = request.form.get('test_score')
+    interview_score = request.form.get('interview_score')
 
-    output = round(prediction[0],2)
+    prediction = model.predict([[int(exp), int(score), int(interview_score)]])
 
-    return render_template("base.html",prediction_text= f"Employee salary will be $ {output}")
+    output = round(prediction[0], 2)
+
+    return render_template('base.html', prediction_text = f"Employee salary will be $ {output}")
 
 
-    return "YOUR FORM IS SUBMITTED"
-
-# @app.route('/contact')
-# def cont():
-#     return "welcome to contact page"
-
-# @app.route('/feedback')
-# def feed():
-#     return "welcome to feedback page"
-
-# @app.route('/help')
-# def help():
-#     return "welcome to help page"
-
-# @app.route('/data science')
-# def ds():
-#     return "welcome to data science page"
-if __name__=="__main__":
+if __name__== '__main__':
     app.run(debug=True)
--- train.py
+++ train.py
@@ -4,22 +4,31 @@ import joblib
 
 from sklearn.linear_model import LinearRegression
 
-dataset=pd.read_csv("hiring.csv")
+dataset = pd.read_csv('hiring.csv')
 print(dataset)
-dataset.experience.fillna(0,inplace=True)
-dataset.test_score.fillna(dataset.test_score.mean(),inplace=True)
-X=dataset.iloc[:,:3]
+
+dataset.experience.fillna(0, inplace = True)
+
+dataset.test_score.fillna(dataset.test_score.mean(), inplace = True)
+
+X = dataset.iloc[: , :3]
+
 def convert_to_int(word):
-    word_dic={"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eiht":8,"nine":9,"ten":10,"eleven":11,0:0}
+    word_dic = {'one':1 , 'two':2 , "three":3 , "four":4 , 'five':5 , 'six':6 , "seven":7 , 'eight':8 , 'nine':9 , 'ten':10 , 'eleven':11 , 0:0}
     return word_dic[word]
 
-X["experience"]=X.experience.apply(lambda x : convert_to_int(x))
+X['experience'] = X.experience.apply(lambda x : convert_to_int(x))
 
-y=dataset.iloc[:,-1]
+y = dataset.iloc[: , -1]
 
-regressor=LinearRegression()
+
+regressor = LinearRegression()
+
 regressor.fit(X,y)
-print("Model training is done")
 
-joblib.dump(regressor,"hiring_model.pkl")
-print(regressor.predict([[1,8,9]]))
+print('Model Training is done')
+
+joblib.dump(regressor , 'hiring_model.pkl',)
+
+
+print(regressor.predict([[1 , 8,9]]))
